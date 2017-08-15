1d9a70f93b1124f8e4e5bda905dd940b55dd3984
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-08-15 15:35:41 +0200

    Check if the user is authenticated

--- client/components/Header.js
+++ client/components/Header.js
@@ -3,13 +3,31 @@ import { graphql } from 'react-apollo'; //connect the query with the component
 import query from '../queries/CurrentUser';
 
 class Header extends Component {
+	
+	renderButtons() {
+		const { loading, user } = this.props.data;
+
+		if (loading) { return <div />; }
+
+		if (user) {
+			<div>Logout</div>
+		} else {
+			return (
+				<div>You're not signed in.</div>
+			)
+		}
+
+	}
+
 	render() {
-		console.log(this.props.data); //the result of the query always exists on 'this.props.data'
+		// console.log(this.props.data); //the result of the query always exists on 'this.props.data'
 		
 		return (
-			<div>
-				Header
-			</div>
+			<nav>
+				<div className="nav-wrapper">
+					{this.renderButtons()}
+				</div>
+			</nav>
 		);
 	}
 }
