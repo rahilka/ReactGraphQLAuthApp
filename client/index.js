65cfaf84b9cb1085171d4e4dacc39fe413ab073c
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-08-15 12:27:22 +0200

    Setup apollo client and provider

--- client/index.js
+++ client/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// here we will setup and start the React Router and the Apollo client and provider
+
+import ApolloClient from 'apollo-client';	//this is default export
+import { ApolloProvider } from 'react-apollo';	//ApolloProvider is the glue between the React and ApolloClient
+
+const client = new ApolloClient({
+	dataIdFromObject: o => o.id	//every single record that comes from the backend will have an ID property defined on it
+});
+
 const Root = () => {
   return (
-    <div>
-      Auth Starter
-    </div>
+  	<ApolloProvider client={client}>
+	    <div>
+	      Auth Starter
+	    </div>
+	</ApolloProvider>
   );
 };
 
